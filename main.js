bf55107d0b7a534e962568286fb00ed2fc4ca493
JemjemfromYT <saavedrajames10@gmail.com>
2025-04-20 17:26:10 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,135 @@
+let inputIdScript = document.getElementById("inputId")
+let buttonIdScript = document.getElementById("buttonId")
+let outputIdScript = document.getElementById("outputId")
+let buttonIdScript2 = document.getElementById("buttonId2")
+let outputIdScript2 = document.getElementById("outputId2")
+let outputIdScript3 = document.getElementById("outputId3")
+let buttonIdScript3 = document.getElementById("buttonId3")
+let inputIdScript2 = document.getElementById("inputId2")
+let outputIdScript4 = document.getElementById("outputId4")
+let outputIdScript5 = document.getElementById("outputId5")
+let chenyaoPicIdScript = document.getElementById("chenyaoPicId")
+let outputChenyaoIdScript = document.getElementById("outputChenyaoId")
+const audio = document.getElementById("audioHorrorId");
+let buttonIdScript4 = document.getElementById("buttonId4")
+    
+   
+
+
+
+buttonIdScript.addEventListener("click", function(){
+  
+  
+  
+outputIdScript.textContent = "Welcome " + inputIdScript.value, 2000;
+buttonIdScript2.style.display = "inline", 2000;
+
+buttonIdScript2.addEventListener("click", function(){
+
+alert("Warning")
+alert("this may contain skibidi")
+
+outputIdScript2.textContent=("fake loading....")
+
+setTimeout(function(){
+  
+  outputIdScript2.textContent=("please wait..")
+},4000);
+
+setTimeout(function(){
+  outputIdScript2.style.display="none"
+},7000);
+
+setTimeout(function(){
+  document.body.style.backgroundColor="red"
+  
+  alert("Warning!")
+  alert("if your answer is wrong, I will manipulate your system")
+  
+  const a = "a.1 \nb.1 \nc.11 \nd.0"
+  inputIdScript2.style.display="inline";
+  buttonIdScript3.style.display="inline"
+  
+  outputIdScript3.textContent=("Question number 1: what is the answer of 1 + 1?"+ "\na.0" + "\nb.2" + "\nc.11" + "\nd.1" );
+  
+  buttonIdScript3.addEventListener("click", function(){
+    
+    
+    if(inputIdScript2.value == "c"){
+      outputIdScript5.textContent = "Congrats"
+    }
+    
+    else{
+    outputIdScript5.textContent ="you are wrong";
+    setTimeout(function (){
+      outputIdScript5.textContent = "I will punish you. \n feyk loading....."
+      alert("💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀")
+    },1500);
+    
+    setTimeout(() => {
+      outputIdScript5.textContent="you know why your wrong?"
+    }, 6000)
+    setTimeout(() => {
+      outputIdScript5.textContent="because 1+1 has no equal"
+    }, 9000)
+    setTimeout(() => {
+      outputIdScript5.textContent="so its letter c. 11🖕"
+      
+      setTimeout(function(){
+      chenyaoPicIdScript.src='chenyao.jpeg'
+      chenyaoPicIdScript.style.display="inline";
+      
+      setTimeout(function(){
+      chenyaoPicIdScript.src='chenyao2.jpeg'
+      chenyaoPicIdScript.style.display="inline";
+      outputChenyaoIdScript.textContent="you did a shitty job, better touch grass next time🖕";
+      
+      setTimeout(function(){
+   buttonIdScript4.style.display="inline";
+   buttonIdScript4.addEventListener("click", function(){
+     document.body.style.backgroundImage = "url('virus.gif')";
+     
+      const audio = document.getElementById("audioHorrorId");
+    audio.play();
+    
+    
+   });
+   
+   
+
+
+
+    
+
+   
+      },4000);
+   
+    },3000);
+      
+    },2000);
+      
+    }, 12000)
+    
+    
+    
+    
+    
+    }
+    
+  });
+  
+  
+  
+  
+  
+  
+  
+  
+}, 9000);
+
+
+
+
+  
+});
+});
